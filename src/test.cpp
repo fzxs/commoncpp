b771691b06051c9ab999a50e4e7e5191f66fbe93
zhanggf <zhanggf@ynt.ai>
2018-10-11 11:26:18 +0800

    更新stringbuilder类

---
+++ src/test.cpp
@@ -0,0 +1,32 @@
+
+#include <iostream>
+#include "gtl_stringbuilder.h"
+
+using namespace std;
+
+
+void test()
+{
+	int i = 0;
+	gtl::TLStringBuilder<char> builder;
+
+	for (i = 0; i < 10; i++)
+	{
+		string str("hello");
+		builder.AppendLine(str);
+	}
+
+	cout << builder.ToString() << endl;
+}
+
+int main()
+{
+	test();
+	getchar();
+	return 0;
+}
+
+
+
+
+
